c6c818b6a26dd6d568d3449a6c3971996fc5d092
iwdok <ivdok23@gmail.com>
2018-06-04 19:50:35 +0500

    Версия 1

---
+++ Alogms/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Alogms
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clearElems_Click(object sender, RoutedEventArgs e)
+        {
+            set.Clear();
+        }
+
+        private void clearSubsets_Click(object sender, RoutedEventArgs e)
+        {
+            subsets.Clear();
+        }
+
+        private void clearSet_Click(object sender, RoutedEventArgs e)
+        {
+            set.Clear();
+            subsets.Clear();
+        }
+
+        private void autofill_Click(object sender, RoutedEventArgs e)
+        {
+            set.Clear();
+
+            int startEl = int.Parse(startElement.Text);
+            int endEl = int.Parse(endElement.Text);
+            if (startEl <= endEl)
+            {
+                for (int i = startEl; i <= endEl; i++)
+                {
+                    set.AppendText(i.ToString() + Environment.NewLine);
+                }
+            }
+            else
+            {
+                for (int i = startEl; i >= endEl; i--)
+                {
+                    set.AppendText(i.ToString() + Environment.NewLine);
+                }
+            }
+            set.ScrollToEnd();
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = int.Parse(sumBox.Text);
+
+            var buf = set.Text.Split(new Char[] { ' ', ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] arraySet = new int[buf.Length];
+
+            for (int i = 0; i < arraySet.Length; i++)
+            {
+                if (!int.TryParse(buf[i], out arraySet[i]))
+                {
+                    MessageBox.Show("Превышено максимально допустимое значение числа " + buf[i]);
+                    return;
+                }
+            }
+
+            subsets.Clear();
+
+            var subSetSum = new SubSetSum();
+            subSetSum.Search(arraySet, sum);
+
+            if (subSetSum.Subsets.Count == 0 || subSetSum.Subsets[0].Count == 0)
+            {
+                subsets.AppendText("Подмножеств с заданной суммой нет");
+            }
+            else
+            {
+                string output;
+                foreach (var subset in subSetSum.Subsets)
+                {
+                    output = "{ ";
+                    for (int i = 0; i < subset.Count(); i++)
+                    {
+                        output += subset[i].ToString();
+                        if (i != subset.Count() - 1)
+                        {
+                            output += ",";
+                        }
+                        output += " ";
+                    }
+                    output += "}" + Environment.NewLine;
+                    subsets.AppendText(output);
+                }
+                countSubsets.Text = subSetSum.Subsets.Count().ToString();
+                subsets.ScrollToEnd();
+            }
+        }
+
+        #region Запрет ввода ненужных символов в поля
+        private void OnPreviewTextInputDigits(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(IsDigits);
+        }
+        private void OnPastingDigits(object sender, DataObjectPastingEventArgs e)
+        {
+            var stringData = (string)e.DataObject.GetData(typeof(string));
+            if (stringData == null || !stringData.All(IsDigits))
+                e.CancelCommand();
+        }
+        
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(IsGood);
+        }
+        private void OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            var stringData = (string)e.DataObject.GetData(typeof(string));
+            if (stringData == null || !stringData.All(IsGood))
+                e.CancelCommand();
+        }
+
+        private bool IsGood(char c)
+        {
+            if (Char.IsDigit(c) || c == ',' || c == '\n' || c == '\r')
+                return true;
+            return false;
+        }
+
+        private bool IsDigits(char c)
+        {
+            if (Char.IsDigit(c) && !Char.IsWhiteSpace(c))
+                return true;
+            return false;
+        }
+
+        private void textBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+        #endregion
+
+    }
+}
---
+++ Alogms/SubSetSum.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace Alogms
+{
+    internal class SubSetSum
+    {
+        public List<List<int>> Subsets { get; private set; }
+
+        private bool[,] solution = null; // матрица
+        private int[] set = null;
+
+        public void Search(int[] set, int sum)
+        {
+            Subsets = new List<List<int>>();
+            this.set = set;
+
+            if (set.Length == 0 || sum < 0)
+            {
+                return;
+            }
+
+            BuildingSolution(sum);
+
+            if (solution[set.Length - 1, sum] == false)
+            {
+                return;
+            }
+            
+            List<int> p = new List<int>();
+            SubsetsRec(set.Length - 1, sum, p);
+        }
+        
+        private void BuildingSolution(int sum)
+        {
+            solution = new bool[set.Length, sum + 1];
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                solution[i, 0] = true;
+            }
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                for (int j = 1; j <= sum; j++)
+                {
+                    if (i != 0)
+                    {
+                        solution[i, j] = solution[i - 1, j];
+
+                        if (solution[i, j] == false && j >= set[i])
+                            solution[i, j] = solution[i, j] || solution[i - 1, j - set[i]];
+                    }
+                    else
+                    {
+                        if (j == set[i])
+                        {
+                            solution[i, j] = true;
+                        }
+                        else
+                        {
+                            solution[i, j] = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SubsetsRec(int i, int sum, List<int> p)
+        {
+            if (i == 0 && sum != 0 && solution[0, sum])
+            {
+                p.Add(set[i]);
+                Subsets.Add(p);
+                return;
+            }
+
+            if (i == 0 && sum == 0)
+            {
+                Subsets.Add(p);
+                return;
+            }
+
+            if (i != 0 && solution[i - 1, sum])
+            {
+                List<int> b = p.GetRange(0, p.Count);
+                SubsetsRec(i - 1, sum, b);
+            }
+
+            if (i != 0 && sum >= set[i] && solution[i - 1, sum - set[i]])
+            {
+                p.Add(set[i]);
+                SubsetsRec(i - 1, sum - set[i], p);
+            }
+        }
+    }
+}
